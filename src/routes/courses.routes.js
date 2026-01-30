7116dab0c528d409f40f05f3ff9fa48571c1cf2a
Ali Ravanbakhsh <ravanbakhsh.ali@gmail.com>
2026-01-29 20:53:52 -0500

    feat: implement getCourseById endpoint, fix router and prisma imports

--- src/routes/courses.routes.js
+++ src/routes/courses.routes.js
@@ -5,4 +5,6 @@ const courseController = require('../controllers/course.controller');
 // GET /api/courses
 router.get('/', courseController.getAllCourses);
 
+router.get('/:id', courseController.getCourseById);
+
 module.exports = router;
